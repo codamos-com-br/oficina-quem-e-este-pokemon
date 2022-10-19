c972c950e8b5f08c955149cc9bc4bdce0234ab6e
Nawarian <nickolas@phpsp.org.br>
2022-10-20 01:29:21 +0200

    test(IndexPage) check for h2 tag

---
+++ tests/IndexPageTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class IndexPageTest extends WebTestCase
+{
+    public function testTitleUsesH1Tag(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Quem é este Pokémon?');
+    }
+}

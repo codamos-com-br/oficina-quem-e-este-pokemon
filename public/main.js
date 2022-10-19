e9764c001be037600de62af22fd28d9af506f6c5
Nawarian <nickolas@phpsp.org.br>
2022-10-20 00:15:12 +0200

    Deixa ponto de entrada para oficina

--- public/main.js
+++ public/main.js
@@ -5,30 +5,6 @@
     window.document.querySelector('form.pokemon-guess').addEventListener('submit', submitGuess);
   }
 
-  async function loadMysteriousPokemon() {
-    const res = await fetch('/current');
-
-    if (res.ok) {
-      const src = await res.text();
-      window.document.querySelector('img.pokemon-img').src = src;
-    } else {
-      // Make pokemon img frame red
-      window.document.querySelector('main.framed').classList.add('danger');
-      window.document.querySelector('img.pokemon-img').parentElement.innerHTML = `
-        <p>
-          Não foi possível carregar o Pokémon de hoje.
-        </p>
-      `;
-
-      // Add gray overlay to guess box
-      window.document.querySelector('aside.framed').classList.add('disabled');
-
-      // Disable guess form
-      window.document.querySelector('input[name="name"]').disabled = true;
-      window.document.querySelector('button[type="submit"]').disabled = true;
-    }
-  }
-
   async function submitGuess(event) {
     event.preventDefault();
 
@@ -36,7 +12,7 @@
     const guess = input.value;
 
     if (guesses.find((g) => g === guess)) {
-      console.warn(`Guess ${guess} already submited. Ignoring.`);
+      console.warn(`Palpite ${guess} já foi enviado. Ignorando.`);
       guess.value = '';
       return;
     }
@@ -45,19 +21,13 @@
     const html = guesses.map((guess) => `<li>${guess}</li>`).reverse().join("\n");
     window.document.querySelector('ul#guesses').innerHTML = html;
 
-    try {
-      const res = await fetch(`/submit?guess=${guess}`, { method: 'POST' });
-
-      if (res.ok) {
-        window.document.querySelector('ul#guesses li:first-child').append(' ✅');
-      }
-    } catch (err) {
-      console.error(`Failed to submit guess`, err);
-    }
+    /**
+     * @TODO: Enviar palpite ao servidor e indicar sucesso/falha.
+     * A variável `guess: string` é contém o valor enviado pelo usuário.
+     */
   }
 
   async function handleContentLoaded() {
-    // await loadMysteriousPokemon();
     attachEventListeners();
   }
 
